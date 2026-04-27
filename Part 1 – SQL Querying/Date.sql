3a4c5e55f87a19d6ce6bd5a89b532857df1a4610
Hao My Tran <106227875+HaoMy-Tran@users.noreply.github.com>
2026-04-27 12:15:45 +0700

    Update Date.sql

--- Part 1 – SQL Querying/Date.sql
+++ Part 1 – SQL Querying/Date.sql
@@ -1,4 +1,16 @@
 SELECT
   Date
 FROM
-  UNNEST(GENERATE_DATE_ARRAY('2019-01-01', '2025-09-30', INTERVAL 1 MONTH)) AS Date
+  UNNEST(
+    GENERATE_DATE_ARRAY(
+      (SELECT 
+        DATE(MIN(oi.delivered_at))
+      FROM `business-analysis-exercise.thelook_ecommerce.order_items` AS oi
+      ), 
+      (SELECT 
+        DATE(MAX(oi.delivered_at))
+      FROM `business-analysis-exercise.thelook_ecommerce.order_items` AS oi
+      ), 
+      INTERVAL 1 MONTH
+    )
+  ) AS Date
