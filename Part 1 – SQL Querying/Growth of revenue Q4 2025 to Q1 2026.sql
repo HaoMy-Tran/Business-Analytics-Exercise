94d39cb9f5e715e08f8dae5db403bd3799b3bc5c
Hao My Tran <106227875+HaoMy-Tran@users.noreply.github.com>
2026-04-27 12:18:24 +0700

    Add SQL file for revenue growth analysis Q4 2025 to Q1 2026

---
+++ Part 1 – SQL Querying/Growth of revenue Q4 2025 to Q1 2026.sql
@@ -0,0 +1,22 @@
+SELECT 
+  (
+    SUM(sub1.Q12026_revenue)/
+    SUM(sub1.Q42025_revenue) - 1
+  )*100 growth_percent
+FROM (
+SELECT 
+	FORMAT_TIMESTAMP('%B %Y',d.Date) as Month,
+  sub.Q42025_revenue,
+  sub.Q12026_revenue
+FROM `thelook_ecommerce.Date` d
+LEFT JOIN (
+  SELECT 
+    FORMAT_TIMESTAMP('%B %Y',delivered_at) as Month,
+    SUM(CASE WHEN oi.delivered_at >= '2025-10-01' and oi.delivered_at <= '2025-12-31' THEN sale_price ELSE 0 END) Q42025_revenue,
+    SUM(CASE WHEN oi.delivered_at >= '2026-01-01' and oi.delivered_at <= '2026-03-31' THEN sale_price ELSE 0 END) Q12026_revenue
+  FROM `thelook_ecommerce.order_items` oi
+  WHERE oi.status = 'Complete'
+  GROUP BY Month
+) as sub
+ON sub.Month = FORMAT_TIMESTAMP('%B %Y',d.Date)
+) sub1
--- Part 1 – SQL Querying/Share of revenue of top categories over total.sql
+++
@@ -1,4 +0,0 @@
-SELECT SUM(Revenue)/
-  (SELECT SUM(Total_revenue) FROM `thelook_ecommerce.5_Top Categories in Last Quarter `
-) as Share_of_revenue
-FROM `thelook_ecommerce.Total revenue in last quarter`
